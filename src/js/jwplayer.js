777bc4b3b5454512fd24cb82a3e1f0280809cd90
Alexandra Chirita <alex@whatchado.com>
2016-05-30 15:53:26 +0200

    test repo cloning

--- src/js/jwplayer.js
+++ src/js/jwplayer.js
@@ -2,6 +2,7 @@ define([
     'api/global-api',
     'utils/helpers'
 ], function (GlobalApi, utils) {
+    // test cloned repo
     /*global __webpack_public_path__:true*/
     __webpack_public_path__ = utils.loadFrom();
 
